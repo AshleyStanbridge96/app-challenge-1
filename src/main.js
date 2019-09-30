b985bcaeadba2e127f296da3d370010fec796f67
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-09-30 12:27:11 -0500

    Added logic to js file to find length of word. Added pre-made layout for index.html

--- src/main.js
+++ src/main.js
@@ -1,37 +1,25 @@
 //Function layout
-const calcTotal = (billAmt, tipPer, numPeople) => {
-    //Divide by the number of people to find total amount for each person
-    const newTotal = (billAmt / numPeople);
-
-    //Find tip amount for person(s)
-    const tipAmt = newTotal * tipPer;
-
-    //Overall total with tip included
-    const overallTotal = newTotal + tipAmt;
-
-    return overallTotal;
+const calcstringLength = (firstString) => {
+    const stringLength = firstString.length;
+    return stringLength;
 };
 
 
 document.querySelector("#calcButton").addEventListener("click", () => {
-    //Declare variables here and convert strings to numbers or floats
-    //Replace ID with correct name
-    const bAmt = parseFloat(document.querySelector("#bill-amt").value);
-    const tAmt = parseFloat(document.querySelector("#tipPercent").value);
-    const pAmt = parseInt(document.querySelector("#numPeople").value);
+    //Grabbing value of string
+    const firstString = document.querySelector("#firstString").value;
 
     //Begin of calculations
-    //Declare variables to store output of calculations
-    const billTotal = Math.round(calcTotal(bAmt, tAmt, pAmt) * 100) / 100;
-    const tipTotal = Math.round(calcTip(bAmt, tAmt, pAmt) * 100) / 100;
-
-    //Creating variables to be sent to IDs below
-    const tip = `Tip Total: $ ${tipTotal}`;
-    const total = `Bill Total: $ ${billTotal}`
-
+    const stringLength = calcstringLength(firstString);
+    const characterLength = `The length of this string is ${stringLength} characters`;
+    
     //Send results back to IDs
-    document.querySelector("#finalTipAmt").innerHTML = tip;
-    document.querySelector("#finalTotalAmt").innerHTML = total;
-    document.querySelector("#result").innerHTML = total;
+
+    if(stringLength < 2){
+        document.querySelector("#result").innerHTML = "Please put a word that contains at least 2 characters";
+    }
+    else{
+        document.querySelector("#result").innerHTML = characterLength;
+    }
 
 });
